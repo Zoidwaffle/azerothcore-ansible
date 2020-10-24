4317e83bbe8a59c4bbc565f28a44612574929433
Zoidwaffle <zoidwaffle@gmail.com>
2020-10-24 13:51:46 +0200

    Make the backup script a bit more generic

--- roles/azerothcore/templates/sql/backup.sh
+++ roles/azerothcore/templates/sql/backup.sh
@@ -17,3 +17,29 @@ backup "{{ azerothcore_db_characters }}"
 backup "{{ azerothcore_db_auth }}"
 
 echo "Backups complete!"
+#!/bin/bash
+
+#####################
+# Backup databases
+#####################
+
+# The destination will default to current location if no variable is given
+DESTINATION="$1"
+
+TIMESTAMP="$(date +"%s")"
+
+if [[ -z "${DESTINATION}" ]]; then
+  DESTINATION="."
+fi
+
+backup(){
+  echo -n "Making a backup of database \"$1\": "
+  mysqldump "${1}" | gzip > "${DESTINATION}/${1}.${TIMESTAMP}.sql.gz" || echo "Failed"
+  echo "Done"
+}
+
+backup "acore_world"
+backup "acore_characters"
+backup "acore_auth"
+
+echo "Backups complete!"
