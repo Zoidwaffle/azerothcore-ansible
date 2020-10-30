6b5e3a96bd2bca042466d51898d7799b46248c73
Zoidwaffle <zoidwaffle@gmail.com>
2020-10-30 17:25:20 +0100

    Update the 'auth' database configuration a bit more

--- roles/azerothcore/templates/sql/auth_custom.sql
+++ roles/azerothcore/templates/sql/auth_custom.sql
@@ -1,2 +1,2 @@
 -- Changes to this file will be overwritten, make changes in ./group_vars/all
-update realmlist set name="{{ azerothcore_realmlist_name }}", localAddress="{{ azerothcore_realmlist_ip }}" where id=1;
+update realmlist set name="{{ azerothcore_realmlist_name }}", localAddress="{{ azerothcore_realmlist_local_ip }}", address="{{ azerothcore_realmlist_ip }}" where id=1;
