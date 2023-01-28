2ef209ed7f915c0e2e8a3c84699eba3037b22384
Zoidwaffle <zoidwaffle@gmail.com>
2023-01-28 11:00:55 +0100

    Fix the privilege string

--- roles/azerothcore/templates/sql/create_databases.sql
+++ roles/azerothcore/templates/sql/create_databases.sql
@@ -1,7 +1,7 @@
 -- https://github.com/azerothcore/azerothcore-wotlk/blob/master/data/sql/create/create_mysql.sql
 DROP USER IF EXISTS '{{ azerothcore_db_user|quote }}'@'{{ azerothcore_db_server|quote }}';
 CREATE USER '{{ azerothcore_db_user|quote }}'@'{{ azerothcore_db_server|quote }}' IDENTIFIED BY '{{ azerothcore_db_password|quote }}' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0;
-GRANT ALL PRIVILEGES ON *.* TO '{{ azerothcore_db_user|quote }}'@'{{ azerothcore_db_server|quote }}' WITH GRANT OPTION;;
+GRANT ALL PRIVILEGES ON *.* TO '{{ azerothcore_db_user|quote }}'@'{{ azerothcore_db_server|quote }}' WITH GRANT OPTION;
 CREATE DATABASE {{ azerothcore_db_world }} DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE {{ azerothcore_db_characters }}  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE {{ azerothcore_db_auth }} DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
